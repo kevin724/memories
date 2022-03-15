e25ea6e34805b320d425b15bceb4a7878b63de1a
Kevin Jeong <kevin.jeong97@gmail.com>
2022-03-14 17:07:29 -0700

    update functionality

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,31 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64'
 import useStyles from './styles';
-import { useDispatch } from 'react-redux';
-import { createPost } from '../../actions/posts'
+import { useDispatch, useSelector } from 'react-redux';
+import { createPost, updatePost } from '../../actions/posts'
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({creator: '', title: '', message: '', tags: '', selectedFile: ''});
+  const post = useSelector((state) => currentId ? state.posts.find((post) => post._id === currentId) : null);
   const classes = useStyles();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (post) {
+      setPostData(post);
+    }
+  }, [post])
 
   const handleSubmit = (e) => {
     // prevent refresh on browser
     e.preventDefault();
-
-    dispatch(createPost(postData));
+    if (currentId) {
+      // if post exists, it is an edit
+      dispatch(updatePost(currentId, postData));
+    } else {
+      dispatch(createPost(postData));
+    }
+    clear();
   }
 
   const clear = () => {
-
+    setCurrentId(null);
+    setPostData({creator: '', title: '', message: '', tags: '', selectedFile: ''});
   }
 
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
         <Typography variant="h6">
-          Creating a Memory
+          { currentId ? 'Editing' : 'Creating' } a Memory
         </Typography>
         <TextField
         name="creator"
