8ed9dd683c6c7cfc6cb3a80450d5fcd1b03baee8
Kevin Jeong <kevin.jeong97@gmail.com>
2022-03-16 12:46:14 -0700

    delete, like and deploy server

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
@@ -12,9 +14,12 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const PORT = process.env.PORT|| 5000;
-const CONNECTION_URL = 'mongodb+srv://kevin724:kevin970724@cluster0.9sjwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+  res.send('Memories API');
+})
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const PORT = process.env.PORT;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
